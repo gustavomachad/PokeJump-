17cb893b4674b74f07a1c7f152e6ce964b4115d2
gustavomachad <gustavoiogurte@gmail.com>
2022-07-03 14:23:28 -0300

    Evento de erro adicionado, caso o jogador erre o jogo vai parar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const pokemon = document.querySelector('.pokemon')
+const arbusto = document.querySelector('.arbusto')
+
 
 const pulo= () => {
     pokemon.classList.add('pulo');
@@ -7,4 +9,19 @@ const pulo= () => {
         pokemon.classList.remove('pulo');
     }, 500);
 }
+const loop = setInterval(() => {
+ 
+    const arbustoPosi = arbusto.offsetLeft;
+    const pokemonPosi = +window.getComputedStyle(pokemon).bottom.replace('px', '');
+
+    if (arbustoPosi < 260 && arbustoPosi >0 && pokemonPosi <= 100) {
+
+        arbusto.style.animation = 'none';
+        arbusto.style.left = `${arbustoPosi}px`;
+        
+        pokemon.style.animation = 'none';
+        pokemon.style.bottom = `${pokemonPosi}px`;
+    }
+}, 10)
+
 document.addEventListener('keydown', pulo);
